3fa52a4ddde6db0faef7f0f00a101727c1ae6c37
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-09-15 15:30:11 +0200

    ref: initial changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+import type { AppProps } from 'next/app';
+
+import '@/styles/globals.scss';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>network-client</title>
+      </Head>
+      <main className={`${inter.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
