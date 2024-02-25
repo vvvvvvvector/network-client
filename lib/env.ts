6e692e916a71096cab135ecaff884dc1816f0acd
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-02-25 15:48:14 +0300

    fix: now NEXT_PUBLIC env variables can be used on a client

--- lib/env.ts
+++ lib/env.ts
@@ -1,35 +1,35 @@
 import { z } from 'zod';
 
-// todo: BASE_URL: string | undefined, but i don't how to create such a type with zod
-// not: BASE_URL?: string | undefined
+const envVariablesSchema = z
+  .object({
+    BASE_URL: z
+      .string()
+      .min(1, { message: 'env variable BASE_URL is required' })
+      .url({ message: 'Invalid BASE_URL' })
+      .optional(),
+    NEXT_PUBLIC_SIGN_IN_PASSWORD: z.string().optional(),
+    NEXT_PUBLIC_API_URL: z
+      .string()
+      .min(1, { message: 'env variable NEXT_PUBLIC_API_URL is required' })
+      .url({ message: 'Invalid NEXT_PUBLIC_API_URL' })
+      .optional(),
+    NEXT_PUBLIC_TOKEN_NAME: z
+      .custom<`${string}.token`>(
+        (val) => (typeof val === 'string' ? /^.+.token$/.test(val) : false),
+        {
+          message:
+            'Invalid token name, must be: [whatever at least one symbol].token'
+        }
+      )
+      .optional()
+  })
+  .passthrough();
 
-const envVariablesSchema = z.object({
-  BASE_URL: z
-    .string()
-    .min(1, { message: 'env variable BASE_URL is required' })
-    .url({ message: 'Invalid BASE_URL' })
-    .optional(),
-  NEXT_PUBLIC_SIGN_IN_PASSWORD: z.string().optional(),
-  NEXT_PUBLIC_API_URL: z
-    .string()
-    .min(1, { message: 'env variable NEXT_PUBLIC_API_URL is required' })
-    .url({ message: 'Invalid NEXT_PUBLIC_API_URL' })
-    .optional(),
-  NEXT_PUBLIC_TOKEN_NAME: z
-    .custom<`${string}.token`>(
-      (val) => (typeof val === 'string' ? /^.+.token$/.test(val) : false),
-      {
-        message:
-          'Invalid token name, must be: [whatever at least one symbol].token'
-      }
-    )
-    .optional()
+// export const env = envVariablesSchema.parse(process.env) // this will not work. you can't destruct process.env as a regular object. only used env variables (like below) are included in the build
+
+export const env = envVariablesSchema.parse({
+  BASE_URL: process.env.BASE_URL,
+  NEXT_PUBLIC_SIGN_IN_PASSWORD: process.env.NEXT_PUBLIC_SIGN_IN_PASSWORD,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_TOKEN_NAME: process.env.NEXT_PUBLIC_TOKEN_NAME
 });
-
-export const env = envVariablesSchema.parse(process.env);
-
-// declare global {
-//   namespace NodeJS {
-//     interface ProcessEnv extends z.infer<typeof envVariablesSchema> {}
-//   }
-// }
