92c2c6faba62812c190732dde158718e31311543
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-02-27 17:21:17 +0300

    refactor: pagination refactor, get rid of useFrequentlyUsedHooks hook completely

--- hooks/use-frequently-used-hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-import { toast as sonnerToast } from 'sonner';
-
-export const useFrequentlyUsedHooks = () => ({
-  router: useRouter(),
-  toast: sonnerToast
-});
