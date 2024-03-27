43fc985493fecafaeb0c49b840bbe2d7419376fd
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-03-27 19:19:35 +0300

    feat: add find friends page

--- app/(authorised)/friends/api.ts
+++ app/(authorised)/friends/api.ts
@@ -1,9 +1,23 @@
 import { request } from '@/app/server';
 
+import type { RequestStatus } from '@/axios/friends';
+
 import type { UserFromListOfUsers } from '@/lib/types';
 
 export async function getMyFriends() {
   return request<UserFromListOfUsers[]>(`friend-requests/accepted`);
+}
+
+export async function getNetworkUsersUsernames(
+  page: string,
+  searchValue?: string
+) {
+  const searchQuery = searchValue ? `&username=${searchValue}` : '';
+  return request<{
+    limit: number;
+    pages: number;
+    users: (UserFromListOfUsers & { requestStatus: RequestStatus })[];
+  }>(`friend-requests/find?page=${page}` + searchQuery);
 }
 
 export async function getIncomingFriendRequests() {
